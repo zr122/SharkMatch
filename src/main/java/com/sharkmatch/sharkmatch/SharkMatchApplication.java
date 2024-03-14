42cdc6b599da1bbb9f2603c87ef7ca440b03a831
zr122 <1297195923@qq.com>
2024-03-14 19:24:03 +0800

    新建了文件夹，但是里面只加了一些依赖

---
+++ src/main/java/com/sharkmatch/sharkmatch/SharkMatchApplication.java
@@ -0,0 +1,13 @@
+package com.sharkmatch.sharkmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharkMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharkmatch/sharkmatch/SharkMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharkmatch.sharkmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
